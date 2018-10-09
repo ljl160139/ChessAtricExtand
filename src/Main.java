08aa318e8803e99b4595c1053fd53622aa89cf4d
ljl160139 <1601396267@qq.com>
2018-10-09 22:17:55 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by kinglong on 2017/7/3.
+ */
+public class Main {
+
+
+    static int columns = 4;  //列
+    static int rows = 5;    //行
+    static int[] markIndex = {16,18,19};   //哪些棋子不能走
+    static int startIndex = 1;    //开始位置
+
+    public static void main(String[] args) {
+        //设置起始位置，查找路径
+        PathFinder.findPath(startIndex, PathFinder.getChessBoard(PathFinder.getChesses(columns, rows, markIndex), columns, rows));
+
+        //不设置其实位置查找全部路径
+        //PathFinder.findAllPath( PathFinder.getChessBoard(PathFinder.getChesses(colums, rows, markIndex), colums, rows));
+    }
+}
